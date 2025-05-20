5684cdebfd320e460a798cf361d9ddf07a2dc550
UtahAvalancheCenter <72477457+UtahAvalancheCenter@users.noreply.github.com>
2025-05-20 12:30:04 -0600

    Update and rename SnowpackValidation-Flat.R to DTW-Atwater-Validation-Code.R

--- DTW-Atwater-Validation-Code.R
+++ DTW-Atwater-Validation-Code.R
@@ -16,3 +16,13 @@ print(Dates)
 # Set date and time for SNOWPACK model to match reference profile date and time in UTC
 ProfileDate <- Dates[enter date and time ID]
 SPpairs$modeled <- snowprofilePro(modeled, ProfileDate = ProfileDate)
+
+# Perform dynamic time warping alignment and check the alignment method used
+dtwAlignment <- dtwSP(SPpairs$modeled, SPpairs$manual, rescale2refHS = FALSE, checkGlobalAlignment = TRUE, simType = "HerlaEtAl2021")
+plotSPalignment(dtwAlignment = dtwAlignment)
+
+# Inspect the local cost density matrix
+plotCostDensitySP(dtwAlignment)
+
+# Check validation scores
+dtwAlignment$sim <- simSP(dtwAlignment$reference, dtwAlignment$queryWarped, verbose = TRUE, type = "HerlaEtAl2021")
